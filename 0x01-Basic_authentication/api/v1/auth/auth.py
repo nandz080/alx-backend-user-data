ba789ca1aaa7e1fc9e059aa3f88c3cdf9b0c7bed
nandz080 <nandz080@gmail.com>
2024-05-21 23:21:22 +0000

    [task 3]

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Module for authentification systems
+"""
+
+from flask import request
+from typing import TypeVar, List
+User = TypeVar('User')
+
+class Auth:
+    """
+    Class serves as template for all authentification systems to be implemented
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Method that returns False -path and excluded_paths
+        """
+        check = path
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != "/":
+            check += "/"
+        if check in excluded_paths or path in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Method that returns None -request
+        """
+        if request is None:
+            return None
+        return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> User:
+        """
+        Method that returns None - request
+        """
+        return None
