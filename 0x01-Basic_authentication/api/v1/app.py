141fc676c55ab76397804323133c652dd5f68eba
nandz080 <nandz080@gmail.com>
2024-05-22 00:40:29 +0000

    [task 6]

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -16,11 +16,12 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-auth_type = os.getenv("AUTH_TYPE", "auth")
-if auth_type == "basic_auth":
-    auth = BasicAuth()
-else:
+auth = None
+
+if getenv("AUTH_TYPE") == "auth":
     auth = Auth()
+elif getenv("AUTH_TYPE") == "basic_auth":
+    auth = BasicAuth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
