b10c3ab51297b0e85ab81e5fff0b274520ad193b
nandz080 <nandz080@gmail.com>
2024-05-23 20:56:07 +0000

    [task 5]

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -42,14 +42,16 @@ def forbidden(error) -> str:
 def before_request():
     """ Method for before_requests handler """
     authorized_list = ['/api/v1/status/',
-                       '/api/v1/unauthorized/', '/api/v1/forbidden/']
-
+                       '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                       '/api/v1/auth_session/login/'
+                       ]
     if auth and auth.require_auth(request.path, authorized_list):
-        if not auth.authorization_header(request):
+        if not auth.authorization_header(request) and not auth.session_cookie(request):
             abort(401)
         request.current_user = auth.current_user(request)
         if request.current_user is None:
             abort(403)
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
