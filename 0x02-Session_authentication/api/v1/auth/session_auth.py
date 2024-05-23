17753668711c7a5da9b20b0db583198f10939cd6
nandz080 <nandz080@gmail.com>
2024-05-23 21:07:02 +0000

    [task 6] Create an instance method def current_user(self, request=None): (overload) that returns a User instance based on a cookie value

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -21,3 +21,14 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ Get the current user """
+        if request is None:
+            return None
+        session_id = self.session_cookie(request)
+        if session_id:
+            user_id = self.user_id_for_session_id(session_id)
+            if user_id:
+                return User.get(user_id)
+        return None
