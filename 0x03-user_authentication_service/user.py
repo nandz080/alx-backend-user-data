d3d5b6c6adc7540f00d6d7481aa6fcf91b04fdcd
nandz080 <nandz080@gmail.com>
2024-05-30 16:17:37 +0000

    [task 0]

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,7 +3,7 @@
 Module for user authentication service.
 """
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -19,3 +19,9 @@ class User(Base):
     hashed_password: Column = Column(String(250), nullable=False)
     session_id: Column = Column(String(250), nullable=True)
     reset_token: Column = Column(String(250), nullable=True)
+
+# Create an engine for the SQLite database (for demonstration purposes)
+engine = create_engine('sqlite:///users.db')
+
+# Create all tables in the engine
+Base.metadata.create_all(engine)
