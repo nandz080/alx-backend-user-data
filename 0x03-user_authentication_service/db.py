6767f956ff5659ee95b0bb943d26f0e3a6a13bf3
nandz080 <nandz080@gmail.com>
2024-05-30 16:44:42 +0000

    [task 1]complete the DB class provided and implement the add_user method

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+Module for DB class
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from typing import TypeVar
+from user import Base, User
+
+
+DATA = ['id', 'email', 'hashed_password', 'session_id', 'reset_token']
+
+
+class DB:
+    """
+    Class that defines db
+    """
+
+    def __init__(self):
+        """Method for initialization of db instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        """Method for memoized sess obj
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """add user to database
+
+        Args:
+            email (string): email of user
+            hashed_password (string): password of user
+        Returns:
+            User: user created
+        """
+        if not email or not hashed_password:
+            return
+        user = User(email=email, hashed_password=hashed_password)
+        session = self._session
+        session.add(user)
+        session.commit()
+        return user
