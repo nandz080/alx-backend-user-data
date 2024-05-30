9229dd4884e0b0fa0150178dfa108bc09b3e215c
nandz080 <nandz080@gmail.com>
2024-05-30 20:06:08 +0000

    [task 12]

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -85,3 +85,21 @@ class Auth:
             self._db.update_user(user.id, session_id=session_id)
         except NoResultFound:
             return
+
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Get a user from a session ID.
+
+        Args:
+            session_id (str): The session ID.
+
+        Returns:
+            User: The user corresponding to the session ID, or None if not found.
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
